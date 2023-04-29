12e07a5cab90641ee46f6cb9c9777e45cb6442ba
Francisco Mauri <labrest03@gmail.com>
2023-04-30 01:51:49 +0200

    edit order

--- resources/js/Admin/Pages/OrderEdit.vue
+++ resources/js/Admin/Pages/OrderEdit.vue
@@ -6,8 +6,8 @@
                 <!--LEFT SIDE-->
                 <div class="w-full lg:w-4/5">
                     <h1 class="font-bold mb-2 text-xl">Order #15445 details</h1>
-                    <p class="font-medium mb-2">Payment via Cash on April 13, 2023 @ 10:12 pm. Customer 24.45.154.153</p>
-                    <div class="flex flex-col sm:flex-row gap-4">
+                    <p class="font-medium mb-6">Payment via Cash on April 13, 2023 @ 10:12 pm. Customer 24.45.154.153</p>
+                    <div class="flex flex-col sm:flex-row gap-6 p-4 border border-gray-200 rounded mb-4">
                         <div class="w-full sm:w-1/5">
                             <h3 class="mb-2 text-lg font-medium">General</h3>
                             <div class="mb-4">
@@ -16,53 +16,206 @@
                             </div>
                             <div class="mb-4">
                                 <label for="content">Status:</label>
-                                <select v-model="order.status" class="p-2 border rounded-sm shadow-sm block w-full">
+                                <select v-model="order.status" class="p-1 border rounded-sm shadow-sm block w-full">
                                     <option value="simple">Pending</option>
                                     <option value="variable">Completed</option>
                                 </select>
                             </div>
                         </div>
                         <div class="w-full sm:w-2/5">
-                            <h3 class="mb-2 text-lg font-medium">Billing</h3>
-                            <div class="flex justify-between mb-4 gap-2">
-                                <div class="w-full">
-                                    <label for="first_name">First Name:</label>
-                                    <input v-model="order.first_name" type="text"
-                                        class="p-2 border rounded-sm shadow-sm block w-full">
+                            <h3 class="mb-2 text-lg font-medium flex">Billing
+                                <svg v-if="billing_preview" class="ml-4 w-4 cursor-pointer"
+                                    @click="billing_preview = !billing_preview" fill="none" stroke="currentColor"
+                                    stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+                                    aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10">
+                                    </path>
+                                </svg>
+                            </h3>
+                            <div v-if="billing_preview">
+                                <p v-text="order.buyer_name"></p>
+                                <p v-text="order.buyer_details.address"></p>
+                                <p>{{ order.buyer_details.state }} {{ order.buyer_details.pc }}</p>
+                                <p v-text="order.buyer_details.email"></p>
+                                <p v-text="order.buyer_details.phone"></p>
+
+                            </div>
+                            <div v-else>
+                                <div class="flex justify-between mb-4 gap-2">
+                                    <div class="w-full">
+                                        <label for="first_name">First Name:</label>
+                                        <input v-model="order.buyer_details.first_name" type="text"
+                                            class="p-1 border rounded-sm shadow-sm block w-full">
+                                    </div>
+                                    <div class="w-full">
+                                        <label for="last_name">Last Name:</label>
+                                        <input v-model="order.buyer_details.last_name" type="text"
+                                            class="p-1 border rounded-sm shadow-sm block w-full">
+                                    </div>
                                 </div>
-                                <div class="w-full">
-                                    <label for="last_name">Last Name:</label>
-                                    <input v-model="order.last_name" type="text"
-                                        class="p-2 border rounded-sm shadow-sm block w-full">
+
+                                <div class="mb-4">
+                                    <label for="address">Street Address:</label>
+                                    <input v-model="order.buyer_details.address" type="text"
+                                        class="p-1 border rounded-sm shadow-sm block w-full">
+                                </div>
+
+                                <div class="flex justify-between mb-4 gap-2">
+                                    <div class="w-full">
+                                        <label for="email">Email:</label>
+                                        <input v-model="order.buyer_details.email" type="text"
+                                            class="p-1 border rounded-sm shadow-sm block w-full">
+                                    </div>
+                                    <div class="w-full">
+                                        <label for="phone">Phone:</label>
+                                        <input v-model="order.buyer_details.phone" type="text"
+                                            class="p-1 border rounded-sm shadow-sm block w-full">
+                                    </div>
+                                </div>
+
+                                <div class="flex justify-between mb-4 gap-2">
+                                    <div class="w-full">
+                                        <label for="suite">Apt name, suite:</label>
+                                        <input v-model="order.buyer_details.suite" type="text"
+                                            class="p-1 border rounded-sm shadow-sm block w-full">
+                                    </div>
+                                    <div class="w-full">
+                                        <label for="town">Town:</label>
+                                        <input v-model="order.buyer_details.town" type="text"
+                                            class="p-1 border rounded-sm shadow-sm block w-full">
+                                    </div>
+                                </div>
+
+                                <div class="flex justify-between mb-4 gap-2">
+                                    <div class="w-full">
+                                        <label for="first_name">State:</label>
+                                        <select class="p-1 border rounded-sm shadow-sm block w-full"
+                                            v-model="order.buyer_details.state">
+                                            <StateSelectOptions></StateSelectOptions>
+                                        </select>
+                                    </div>
+                                    <div class="w-full">
+                                        <label for="pc">Postal code:</label>
+                                        <input v-model="order.buyer_details.pc" type="text"
+                                            class="p-1 border rounded-sm shadow-sm block w-full">
+                                    </div>
                                 </div>
                             </div>
 
-                            <div class="mb-4">
-                                <label for="address">Street Address:</label>
-                                <input v-model="order.address" type="text"
-                                    class="p-2 border rounded-sm shadow-sm block w-full">
-                            </div>
 
-                            <div class="flex justify-between mb-4 gap-2">
-                                <div class="w-full">
-                                    <label for="first_name">First Name:</label>
-                                    <input v-model="order.first_name" type="text"
-                                        class="p-2 border rounded-sm shadow-sm block w-full">
-                                </div>
-                                <div class="w-full">
-                                    <label for="last_name">Last Name:</label>
-                                    <input v-model="order.last_name" type="text"
-                                        class="p-2 border rounded-sm shadow-sm block w-full">
-                                </div>
-                            </div>
-
-                            
 
                         </div>
                         <div class="w-full sm:w-2/5">
+                            <h3 class="mb-2 text-lg font-medium flex">Shipping
+                                <svg v-if="shipping_preview" class="ml-4 w-4 cursor-pointer"
+                                    @click="shipping_preview = !shipping_preview" fill="none" stroke="currentColor"
+                                    stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+                                    aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10">
+                                    </path>
+                                </svg>
+                            </h3>
+                            <div v-if="shipping_preview">
+                                <p>{{ order.buyer_details.sh_first_name }} {{ order.buyer_details.sh_last_name }}</p>
+                                <p v-text="order.buyer_details.sh_address"></p>
+                                <p>{{ order.buyer_details.sh_state }} {{ order.buyer_details.sh_pc }}</p>
+                                <p v-text="order.buyer_details.sh_phone"></p>
+                            </div>
+                            <div v-else>
+                                <div class="flex justify-between mb-4 gap-2">
+                                    <div class="w-full">
+                                        <label for="first_name">First Name:</label>
+                                        <input v-model="order.buyer_details.sh_first_name" type="text"
+                                            class="p-1 border rounded-sm shadow-sm block w-full">
+                                    </div>
+                                    <div class="w-full">
+                                        <label for="last_name">Last Name:</label>
+                                        <input v-model="order.buyer_details.sh_last_name" type="text"
+                                            class="p-1 border rounded-sm shadow-sm block w-full">
+                                    </div>
+                                </div>
+
+                                <div class="mb-4">
+                                    <label for="address">Street Address:</label>
+                                    <input v-model="order.buyer_details.sh_address" type="text"
+                                        class="p-1 border rounded-sm shadow-sm block w-full">
+                                </div>
+
+                                <div class="flex justify-between mb-4 gap-2">
+                                    <div class="w-full">
+                                        <label for="email">Email:</label>
+                                        <input v-model="order.buyer_details.sh_email" type="text"
+                                            class="p-1 border rounded-sm shadow-sm block w-full">
+                                    </div>
+                                    <div class="w-full">
+                                        <label for="phone">Phone:</label>
+                                        <input v-model="order.buyer_details.sh_phone" type="text"
+                                            class="p-1 border rounded-sm shadow-sm block w-full">
+                                    </div>
+                                </div>
+
+                                <div class="flex justify-between mb-4 gap-2">
+                                    <div class="w-full">
+                                        <label for="suite">Apt name, suite:</label>
+                                        <input v-model="order.buyer_details.sh_suite" type="text"
+                                            class="p-1 border rounded-sm shadow-sm block w-full">
+                                    </div>
+                                    <div class="w-full">
+                                        <label for="town">Town:</label>
+                                        <input v-model="order.buyer_details.sh_town" type="text"
+                                            class="p-1 border rounded-sm shadow-sm block w-full">
+                                    </div>
+                                </div>
+
+                                <div class="flex justify-between mb-4 gap-2">
+                                    <div class="w-full">
+                                        <label for="first_name">State:</label>
+                                        <select class="p-1 border rounded-sm shadow-sm block w-full"
+                                            v-model="order.buyer_details.sh_state">
+                                            <StateSelectOptions></StateSelectOptions>
+                                        </select>
+                                    </div>
+                                    <div class="w-full">
+                                        <label for="pc">Postal code:</label>
+                                        <input v-model="order.buyer_details.sh_pc" type="text"
+                                            class="p-1 border rounded-sm shadow-sm block w-full">
+                                    </div>
+                                </div>
+                            </div>
 
                         </div>
                     </div>
+                    <div class="p-4 border border-gray-200 rounded">
+                        <table class="w-full">
+                            <thead>
+                                <tr class="flex p-3 bg-gray-200">
+                                    <th class="w-full text-left">Item</th>
+                                    <th class="w-14 text-left">Cost</th>
+                                    <th class="w-12 text-left">Qty</th>
+                                    <th class="w-14 text-left">Total</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="(item, index) in order.cart_items" :key="index" class="flex p-3">
+                                    <td class="flex gap-2 w-full">
+                                        <img :src="item.image" class="w w-10"/>
+                                        <div>
+                                            <p v-text="item.title"></p>
+                                        </div>
+                                    </td>
+                                    
+                                    <td class="w-14 text-left">{{ item.price }}</td>
+                                    <td class="w-12 text-left">{{ item.quantity }}</td>
+                                    <td class="w-14 text-left">{{ item.quantity }}</td>
+                                </tr>
+                                
+                            </tbody>
+                        </table>
+                    </div>
+
+
                 </div>
                 <!--RIGHT SIDE -->
                 <div class="w-full lg:w-1/5">
@@ -87,6 +240,7 @@ import ConfirmDialog from '../../components/ConfirmDialog.vue'
 import FileUploader from '../components/FileUploader.vue'
 import GlobalTransition from '../../Transitions/GlobalTransition.vue'
 import Datepicker from '@vuepic/vue-datepicker';
+import StateSelectOptions from '../../Components/StateSelectOptions.vue'
 
 import '@vuepic/vue-datepicker/dist/main.css';
 
@@ -94,6 +248,8 @@ const show = ref(false);
 const toaster = createToaster({ position: "top" });
 const dialog = createConfirmDialog(ConfirmDialog)
 const order = ref({ status: 'pending', items: [] })
+const billing_preview = ref(true)
+const shipping_preview = ref(true)
 
 
 
---
+++ resources/js/components/StateSelectOptions.vue
@@ -0,0 +1,83 @@
+<template>
+  <option value="">STATE</option>
+  <option value="Albany-145972" class="table-finder__option">Albany, New York</option>
+  <option value="Long Island-1041478" class="table-finder__option">Long Island, New York
+  </option>
+  <option value="Yonkers-74959" class="table-finder__option">Yonkers, New York</option>
+  <option value="Syracuse-110653" class="table-finder__option">Syracuse, New York</option>
+  <option value="Rochester-213982" class="table-finder__option">Rochester, New York
+  </option>
+  <option value="Buffalo-145969" class="table-finder__option">Buffalo, New York</option>
+  <option value="Fairfax-39403" class="table-finder__option">Fairfax, Virginia</option>
+  <option value="Richmond-39430" class="table-finder__option">Richmond, Virginia</option>
+  <option value="Pittsburgh-95770" class="table-finder__option">Pittsburgh, Pennsylvania
+  </option>
+  <option value="Woodmere-213988" class="table-finder__option">Woodmere, Ohio</option>
+  <option value="San Juan-64201" class="table-finder__option">San Juan, Puerto Rico
+  </option>
+  <option value="Westlake-191053" class="table-finder__option">Westlake, Ohio</option>
+  <option value="Detroit-62785" class="table-finder__option">Detroit, Michigan</option>
+  <option value="Port of Spain-68260" class="table-finder__option">Port Of Spain, Trinidad
+    And Tobago</option>
+  <option value="Columbus-95767" class="table-finder__option">Columbus, Ohio</option>
+  <option value="Milwaukee-991333" class="table-finder__option">Milwaukee, Wisconsin
+  </option>
+  <option value="Lexington-449854" class="table-finder__option">Lexington, Kentucky
+  </option>
+  <option value="Schaumburg-39433" class="table-finder__option">Schaumburg, Illinois
+  </option>
+  <option value="Orland Park-1026676" class="table-finder__option">Orland Park, Illinois
+  </option>
+  <option value="Jacksonville-1008682" class="table-finder__option">Jacksonville, Florida
+  </option>
+  <option value="Palm Beach Gardens-100237" class="table-finder__option">Palm Beach
+    Gardens, Florida</option>
+  <option value="Orlando-39424" class="table-finder__option">Orlando, Florida</option>
+  <option value="Palm Beach-39427" class="table-finder__option">Palm Beach, Aruba</option>
+  <option value="Hallandale Beach-39409" class="table-finder__option">Hallandale Beach,
+    Florida</option>
+  <option value="Huntsville-150787" class="table-finder__option">Huntsville, Alabama
+  </option>
+  <option value="Sunrise – Sawgrass Mills-268636" class="table-finder__option">Sunrise –
+    Sawgrass Mills, Florida</option>
+  <option value="Miami Beach-39418" class="table-finder__option">Miami Beach, Florida
+  </option>
+  <option value="Miami – Dolphin Mall-39421" class="table-finder__option">Miami – Dolphin
+    Mall, Florida</option>
+  <option value="Miami – Dadeland Mall-167458" class="table-finder__option">Miami –
+    Dadeland Mall, Florida</option>
+  <option value="Tampa-60256" class="table-finder__option">Tampa, Florida</option>
+  <option value="Birmingham-115495" class="table-finder__option">Birmingham, Alabama
+  </option>
+  <option value="Memphis-39415" class="table-finder__option">Memphis, Tennessee</option>
+  <option value="Omaha-1258861" class="table-finder__option">Omaha, Nebraska</option>
+  <option value="Rogers-1279333" class="table-finder__option">Rogers, Arkansas</option>
+  <option value="Tulsa-213985" class="table-finder__option">Tulsa, Oklahoma</option>
+  <option value="Baton Rouge-39358" class="table-finder__option">Baton Rouge, Louisiana
+  </option>
+  <option value="Oklahoma City-267856" class="table-finder__option">Oklahoma City,
+    Oklahoma</option>
+  <option value="Tyler-991327" class="table-finder__option">Tyler, Texas</option>
+  <option value="Addison-39355" class="table-finder__option">Addison, Texas</option>
+  <option value="Dallas-39364" class="table-finder__option">Dallas, Texas</option>
+  <option value="Fort Worth-39406" class="table-finder__option">Fort Worth, Texas</option>
+  <option value="Denver-39379" class="table-finder__option">Denver, Colorado</option>
+  <option value="Houston-105568" class="table-finder__option">Houston, Texas</option>
+  <option value="San Antonio-57682" class="table-finder__option">San Antonio, Texas
+  </option>
+  <option value="McAllen-991330" class="table-finder__option">Mcallen, Texas</option>
+  <option value="Tacoma-1026673" class="table-finder__option">Tacoma, Washington</option>
+  <option value="Las Vegas-39412" class="table-finder__option">Las Vegas, Nevada</option>
+  <option value="Fresno-1026667" class="table-finder__option">Fresno, California </option>
+  <option value="Concord-1071352" class="table-finder__option">Concord, California
+  </option>
+  <option value="Rancho Cucamonga-1179205" class="table-finder__option">Rancho Cucamonga,
+    California</option>
+  <option value="Irvine-213994" class="table-finder__option">Irvine, California</option>
+  <option value="Carlsbad-1026670" class="table-finder__option">Carlsbad, California
+  </option>
+  <option value="Oxnard-1050442" class="table-finder__option">Oxnard, California</option>
+</template>
+
+
+
